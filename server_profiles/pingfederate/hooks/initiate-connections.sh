ec1f5a5615da2d7d9f98f3f36457a90c9a72806d
Tam Tran <ttran@pingidentity.com>
2020-09-12 11:57:25 +1000

    original commit

--- server_profiles/pingfederate/hooks/initiate-connections.sh
+++ server_profiles/pingfederate/hooks/initiate-connections.sh
@@ -1,5 +1,5 @@
 cd /opt/out/instance/server/default/data
 rm -R connection-deployer
-git clone ${CONNECTIONDEPLOYER_SERVER_PROFILE}
+git clone ${CONNECTIONDEPLOYER_SERVER_PROFILE} connection-deployer
 
 
