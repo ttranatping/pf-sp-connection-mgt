fa5705588e46cd008c3cf66171f4c82acf236e23
Tam Tran <ttran@pingidentity.com>
2020-09-12 12:03:02 +1000

    original commit

--- server_profiles/pingfederate/hooks/initiate-connections.sh
+++ server_profiles/pingfederate/hooks/initiate-connections.sh
@@ -2,4 +2,4 @@ cd /opt/out/instance/server/default/data
 rm -R connection-deployer
 git clone ${CONNECTIONDEPLOYER_SERVER_PROFILE} connection-deployer
 
-
+cp ${HOOKS_DIR}/update-connections.sh /etc/periodic/15min
