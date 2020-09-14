33ebedef61ae307152b73171291c42bf73ada5bd
Tam Tran <ttran@pingidentity.com>
2020-09-14 10:41:57 +1000

    adding md5 tool

--- scripts/get-md5-from-p12.sh
+++ scripts/get-md5-from-p12.sh
@@ -1,4 +1,5 @@
 
-echo "Usage: ./get-md5-from-p12.sh <path-to-p12-file> <p12-password> <keystore-alias>"
+echo "Usage: ./get-md5-from-p12.sh <path-to-p12-file> <p12-password> <certificate-alias>"
+echo "Hint: PKCS12 keystores exported from PingFederate has certificate-alias=ping"
 
 java -jar ./pf-cert-extract-md5/target/pf-cert-extract-md5-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3
