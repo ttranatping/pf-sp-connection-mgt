702f7b9f98e0192f5e3446046295427a8a5588a0
Tam Tran <ttran@pingidentity.com>
2020-09-14 11:06:25 +1000

    adding md5 tool

--- scripts/export-sp-connection.sh
+++ scripts/export-sp-connection.sh
@@ -14,8 +14,15 @@ bodyContent="<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/e
     </soapenv:Envelope>"
 
 echo 'exporting connection = ' $1
-echo 'substituting with certificate version = ' $2
 
+SigningKeyPairMD5Reference_fingerprint=$(java -jar ./pf-cert-extract-md5/target/pf-cert-extract-md5-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./example-certs/SigningKeyPairReference.p12 2FederateM0re ping)
+DecryptionKeyPairMD5Reference_fingerprint=$(java -jar ./pf-cert-extract-md5/target/pf-cert-extract-md5-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./example-certs/DecryptionKeyPairReference.p12 2FederateM0re ping)
+SecondaryDecryptionKeyPairMD5Reference_fingerprint=$(java -jar ./pf-cert-extract-md5/target/pf-cert-extract-md5-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./example-certs/SecondaryDecryptionKeyPairReference.p12 2FederateM0re ping)
+
+DsigVerificationCert_Base64Encoded=$(cat ./example-certs/DsigVerificationCert.cer | base64)
+SecondaryDsigVerificationCert_Base64EncodedCert=$(cat ./example-certs/SecondaryDsigVerificationCert.cer | base64)
+EncryptionCert_Base64EncodedCert=$(cat ./example-certs/EncryptionCert.cer | base64)
+RoleDescriptor_Base64EncodedCert=$(cat ./example-certs/RoleDescriptor.cer | base64)
 
 curl -X POST \
   --header 'SOAPAction: getConnection' \
@@ -25,11 +32,11 @@ curl -X POST \
   https://localhost:9999/pf-mgmt-ws/ws/ConnectionMigrationMgr --insecure  | \
 xpath '/soapenv:Envelope/soapenv:Body/getConnectionResponse/getConnectionReturn/text()' | \
 sed "s/\&amp;/\&/;s/&lt;/\</;s/&lt;/\</;s/&gt;/\>/;s/&apos;/\'/" | \
-xml ed -u "//urn:SigningKeyPairReference/@MD5Fingerprint" -v "\${SigningKeyPairMD5Reference_$2}" | \
-xml ed -u "//urn:DsigVerificationCert/urn:Base64EncodedCert" -v "\${DsigVerificationBase64EncodedCert_$2}" | \
-xml ed -u "//urn:SecondaryDsigVerificationCert/urn:Base64EncodedCert" -v "\${SecondaryDsigVerificationEncodedCert_$2}" | \
-xml ed -u "//urn:DecryptionKeyPairReference/@MD5Fingerprint" -v "\${DecryptionKeyPairMD5Reference_$2}" | \
-xml ed -u "//urn:SecondaryDecryptionKeyPairReference/@MD5Fingerprint" -v "\${SecondaryDecryptionKeyPairMD5Reference_$2}" | \
-xml ed -u "//urn:EncryptionCert/urn:Base64EncodedCert" -v "\${EncryptionEncodedCert_$2}" | \
-xml ed -u "//md:RoleDescriptor/urn:availableCert/urn:Base64EncodedCert" -v "\${RoleDescriptorEncodedCert_$2}"
+xml ed -u "//urn:SigningKeyPairReference/@MD5Fingerprint" -v "${SigningKeyPairMD5Reference_fingerprint}" | \
+xml ed -u "//urn:DsigVerificationCert/urn:Base64EncodedCert" -v "\${DsigVerificationCert_Base64Encoded}" | \
+xml ed -u "//urn:SecondaryDsigVerificationCert/urn:Base64EncodedCert" -v "\${SecondaryDsigVerificationCert_Base64EncodedCert}" | \
+xml ed -u "//urn:DecryptionKeyPairReference/@MD5Fingerprint" -v "\${DecryptionKeyPairMD5Reference_fingerprint}" | \
+xml ed -u "//urn:SecondaryDecryptionKeyPairReference/@MD5Fingerprint" -v "\${SecondaryDecryptionKeyPairMD5Reference_fingerprint}" | \
+xml ed -u "//urn:EncryptionCert/urn:Base64EncodedCert" -v "\${EncryptionCert_Base64EncodedCert}" | \
+xml ed -u "//md:RoleDescriptor/urn:availableCert/urn:Base64EncodedCert" -v "\${RoleDescriptor_Base64EncodedCert}"
 
--- scripts/get-md5-from-p12.sh
+++ scripts/get-md5-from-p12.sh
@@ -2,4 +2,6 @@
 echo "Usage: ./get-md5-from-p12.sh <path-to-p12-file> <p12-password> <certificate-alias>"
 echo "Hint: PKCS12 keystores exported from PingFederate has certificate-alias=ping"
 
-java -jar ./pf-cert-extract-md5/target/pf-cert-extract-md5-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3
+fingerprint=$(java -jar ./pf-cert-extract-md5/target/pf-cert-extract-md5-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3)
+
+echo "Fingerprint: $fingerprint"
